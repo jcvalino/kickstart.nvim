980b17dd387089832a09904b32edbeda18a4379e
jcvalino <jcvalino@multisyscorp.com>
2025-09-29 14:53:10 +0800

    :zap: use prettierd for formatting js and ts files

--- init.lua
+++ init.lua
@@ -736,7 +736,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
 
         lua_ls = {
@@ -829,9 +829,9 @@ require('lazy').setup({
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
         javascript = { 'prettier' },
-        typescript = { 'prettier' },
+        typescript = { 'prettierd' },
         javascriptreact = { 'prettier' },
-        typescriptreact = { 'prettier' },
+        typescriptreact = { 'prettierd' },
         prisma = { 'prisma_fmt' },
         json = { 'prettier' },
         css = { 'prettier' },
